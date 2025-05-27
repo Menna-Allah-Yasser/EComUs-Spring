615fe41f437e0ed8dd9eaba72b45af62842be31b
abdelrahman <abdelrahman.elsheribini@gmail.com>
2025-05-27 15:32:00 +0300

    Old Daos to JPA repos
    migrated old daos to jpa repos

--- src/main/java/org/iti/ecomus/repository/OrderRepo.java
+++ src/main/java/org/iti/ecomus/repository/OrderRepo.java
@@ -1,7 +1,11 @@
 package org.iti.ecomus.repository;
 
 import org.iti.ecomus.entity.Order;
+import org.iti.ecomus.entity.OrderDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepo extends JpaRepository<Order, Long> {
+
 }
