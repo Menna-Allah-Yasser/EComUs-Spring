f4a49fa8acefc4bf0a8b2db82d357a58f3494853
Ahmed Ashraf <ahmedashraf8228@gmail.com>
2025-05-29 07:41:58 +0300

    Add and test CategoryService
    - Added @ResponseStatus annotations to:
      - ConflictException (@HttpStatus.CONFLICT)
      - ResourceNotFoundException (@HttpStatus.NOT_FOUND)
    
    - Extended CategoryRepo with:
      - existsCategoriesByCategoryName(String name)
    
    - Created CategoryService:
      - Implemented core CRUD operations
      - Integrated validation and conflict checks
      - Fully covered with integration tests
    
    - Enhanced CategoryMapper with:
      - toDtoList(List<Category>)
      - toCategoryList(List<CategoryDTO>)
      - Updated and extended corresponding test class
    
    - Refactored:
      - AddressRepoTest for direct DB interaction
      - CategoryRepoTest for improved structure and clarity

--- src/main/java/org/iti/ecomus/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/iti/ecomus/exceptions/ResourceNotFoundException.java
@@ -1,8 +1,11 @@
 package org.iti.ecomus.exceptions;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 // This class represents a custom exception thrown when a requested resource is not found
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
--- src/main/java/org/iti/ecomus/mappers/CategoryMapper.java
+++ src/main/java/org/iti/ecomus/mappers/CategoryMapper.java
@@ -5,9 +5,13 @@ import org.iti.ecomus.dto.CategoryNoProductDTO;
 import org.iti.ecomus.entity.Category;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
     CategoryDTO toCategoryDTO(Category category);
     CategoryNoProductDTO toCategoryNoProductDTO(Category category);
     Category toCategory(CategoryDTO categoryDTO);
+    List<CategoryDTO> toDtoList(List<Category> categories);
+    List<Category> toCategoryList(List<CategoryDTO> dtos);
 }
