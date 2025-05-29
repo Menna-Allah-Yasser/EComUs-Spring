918363ce2bbf7bc3663b2b967c05e1a93bfb148e
Hazim Karam <hazimkaram159@gmail.com>
2025-05-30 00:34:11 +0300

    Add UserRepo

--- src/main/java/org/iti/ecomus/repository/UserRepo.java
+++ src/main/java/org/iti/ecomus/repository/UserRepo.java
@@ -3,14 +3,22 @@ package org.iti.ecomus.repository;
 import com.sun.jdi.InterfaceType;
 import org.iti.ecomus.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
 
-    Optional<User> findByUserName(String username);
 
+    @Query("SELECT COUNT(u) FROM User u WHERE u.email = :email")
+    Long checkValidEmail(@Param("email") String email);
+
+    User findByEmail(String email);
+
+    Optional<User> findByUserName(String userName);
 }
