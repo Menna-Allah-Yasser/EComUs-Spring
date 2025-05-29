e92457760483c936daf1379b5d30f773647313b0
abdelrahman <abdelrahman.elsheribini@gmail.com>
2025-05-29 19:26:47 +0300

    Removes ResponseStatus annotations
    The `@ResponseStatus` annotations on `ConflictException` and
    `ResourceNotFoundException` are removed. This change allows for
    more flexible and centralized exception handling, potentially
    delegating the responsibility to a global exception handler.

--- src/main/java/org/iti/ecomus/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/iti/ecomus/exceptions/ResourceNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 // This class represents a custom exception thrown when a requested resource is not found
-@ResponseStatus(HttpStatus.NOT_FOUND)
+//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
