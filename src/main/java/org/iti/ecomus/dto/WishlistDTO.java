6173d212ed694878420a964f7466a753a1454e0b
Hazim Karam <hazimkaram159@gmail.com>
2025-06-02 18:14:16 +0300

    modify WishlistDTO

--- src/main/java/org/iti/ecomus/dto/WishlistDTO.java
+++ src/main/java/org/iti/ecomus/dto/WishlistDTO.java
@@ -1,4 +1,12 @@
 package org.iti.ecomus.dto;
 
+import lombok.Data;
+
 public class WishlistDTO {
+    private Long userId;
+    private Long productId;
+    private String productName;
+    private Double productPrice;
+    private String productImage;
+
 }
