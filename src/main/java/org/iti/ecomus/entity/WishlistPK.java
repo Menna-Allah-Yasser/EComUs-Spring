82321630cd2b92b6a31092a3b6874588462d2282
Hazim Karam <hazimkaram159@gmail.com>
2025-06-02 18:11:49 +0300

    modify WishlistPK.java

--- src/main/java/org/iti/ecomus/entity/WishlistPK.java
+++ src/main/java/org/iti/ecomus/entity/WishlistPK.java
@@ -2,21 +2,23 @@ package org.iti.ecomus.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Objects;
 
 @NoArgsConstructor
 @Data
+@AllArgsConstructor
 public class WishlistPK implements Serializable {
 
     private Long userId;
     private Long productId;
 
-    public WishlistPK(Long userId, Long productId) {
-        this.userId = userId;
-        this.productId = productId;
-    }
+//    public WishlistPK(Long userId, Long productId) {
+//        this.userId = userId;
+//        this.productId = productId;
+//    }
 
     @Override
     public boolean equals(Object o) {
