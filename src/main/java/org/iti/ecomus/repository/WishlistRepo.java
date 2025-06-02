df5d603db66750e637bdf005b20cd6a4b07d9aeb
Hazim Karam <hazimkaram159@gmail.com>
2025-06-02 18:09:17 +0300

    addd WishlistRepo Methods

--- src/main/java/org/iti/ecomus/repository/WishlistRepo.java
+++ src/main/java/org/iti/ecomus/repository/WishlistRepo.java
@@ -1,7 +1,18 @@
 package org.iti.ecomus.repository;
 
 import org.iti.ecomus.entity.Wishlist;
+import org.iti.ecomus.entity.WishlistPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WishlistRepo extends JpaRepository<Wishlist, Long> {
+import java.util.List;
+
+public interface WishlistRepo extends JpaRepository<Wishlist, WishlistPK> {
+    // Find all wishlist items for a specific user
+    List<Wishlist> findByUserId(Long userId);
+
+    // Check if a product exists in a user's wishlist
+    boolean existsByUserIdAndProductId(Long userId, Long productId);
+
+    // Delete a specific wishlist item
+    void deleteByUserIdAndProductId(Long userId, Long productId);
 }
